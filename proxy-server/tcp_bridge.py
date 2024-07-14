6ff678b33fd4117839113e52a79cc40bdb302a81
Danial Mobini <danialmobinidh81@gmail.com>
2024-07-14 21:51:06 +0330

    Refactor TCPBridge class for better code organization and readability

--- proxy-server/tcp_bridge.py
+++ proxy-server/tcp_bridge.py
@@ -16,10 +16,12 @@ def threaded(fn):
 
 
 class TCPBridge(object):
-
-    def __init__(self, host, port):
+    def __init__(self, host, port, dst_host, dst_port):
         self.host = host
         self.port = port
+        self.dst_host = dst_host
+        self.dst_port = dst_port
+
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.server.settimeout(1)
@@ -31,7 +33,7 @@ class TCPBridge(object):
             headers = data.decode("utf-8").split("\r\n")
             for header in headers:
                 if header.startswith("Host:"):
-                    return header.split(": ")[1], header.split(":")[2]
+                    return header.split(": ")[1]
         except UnicodeDecodeError:
             pass
         return None
@@ -40,23 +42,24 @@ class TCPBridge(object):
     def tunnel(
         self,
         sock: socket.socket,
+        sock2: socket.socket,
         chunk_size=1024,
+        specific_host="13.13.13.13",
     ):
         try:
             while not self.stop:
-                sock.getpeername()
-                r, w, x = select.select(sock, [], [], 1000)
+                sock.getpeername() and sock2.getpeername()
+                r = select.select([sock, sock2], 1000)
                 if sock in r:
                     data = sock.recv(chunk_size)
                     if len(data) == 0:
                         break
-                    host, port = self.parse_http_request(data)
-                    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                    client_socket.connect(host, port)
-                    client_socket.sendall(data)
+                    host = self.parse_http_request(data)
+                    if host == specific_host:
+                        sock2.sendall(data)
 
-                if client_socket in r:
-                    data = client_socket.recv(chunk_size)
+                if sock2 in r:
+                    data = sock2.recv(chunk_size)
                     if len(data) == 0:
                         break
                     # Assuming you only want to filter requests, not responses
@@ -65,7 +68,7 @@ class TCPBridge(object):
             pass
         finally:
             try:
-                client_socket.close()
+                sock2.close()
             except:
                 pass
             try:
@@ -78,9 +81,12 @@ class TCPBridge(object):
         while not self.stop:
             try:
                 (sock, addr) = self.server.accept()
+                print(f"Connection from {addr}")
                 if sock is None:
                     continue
-                self.tunnel(sock)
+                client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                client_socket.connect((self.dst_host, self.dst_port))
+                self.tunnel(sock, client_socket)
             except KeyboardInterrupt:
                 self.stop = True
             except TimeoutError as exp:
@@ -89,6 +95,19 @@ class TCPBridge(object):
                 print("Exception:", exp)
 
 
+def http_packet_callback(packet):
+    if packet.haslayer(HTTPRequest):
+        http_layer = packet.getlayer(HTTPRequest)
+        print(f"HTTP Method: {http_layer.Method.decode('utf-8')}")
+        print(f"HTTP Host: {http_layer.Host.decode('utf-8')}")
+        if packet.haslayer(TCP):
+            tcp_layer = packet.getlayer(TCP)
+            ip_layer = packet.getlayer(IP)
+            print(
+                f"Source IP: {ip_layer.src}:{tcp_layer.sport} --> Destination IP: {ip_layer.dst}:{tcp_layer.dport}"
+            )
+
+
 if __name__ == "__main__":
-    tcp_bridge = TCPBridge("0.0.0.0", 8080)
+    tcp_bridge = TCPBridge("0.0.0.0", 8080, "192.168.44.130", 80)
     tcp_bridge.run()
