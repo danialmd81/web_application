064dcc95b92480418667460d2c3383e3ca391f00
Danial Mobini <danialmobinidh81@gmail.com>
2024-07-14 22:27:36 +0330

    Refactor TCPBridge class to improve code organization and readability

--- proxy-server/tcp_bridge.py
+++ proxy-server/tcp_bridge.py
@@ -17,11 +17,12 @@ def threaded(fn):
 
 class TCPBridge(object):
 
-    def __init__(self, host, port, dst_host, dst_port):
+    # def __init__(self, host, port, dst_host, dst_port):
+    def __init__(self, host, port):
         self.host = host
         self.port = port
-        self.dst_host = dst_host
-        self.dst_port = dst_port
+        # self.dst_host = dst_host
+        # self.dst_port = dst_port
 
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -39,26 +40,35 @@ class TCPBridge(object):
             pass
         return None
 
+    # @threaded
+    # def tunnel(
+    #     self,
+    #     sock: socket.socket,
+    #     sock2: socket.socket,
+    #     chunk_size=1024,
+    #     specific_host="13.13.13.13",
+    # ):
     @threaded
     def tunnel(
         self,
         sock: socket.socket,
-        sock2: socket.socket,
         chunk_size=1024,
         specific_host="13.13.13.13",
     ):
         try:
             while not self.stop:
-                sock.getpeername() and sock2.getpeername()
-                r, w, x = select.select([sock, sock2], [], [], 1000)
+                sock.getpeername()
+                r, w, x = select.select(sock, [], [], 1000)
                 if sock in r:
                     data = sock.recv(chunk_size)
                     if len(data) == 0:
                         break
                     host = self.parse_http_request(data)
-                    if host == specific_host:
-                        print(sock.getpeername(), ":", sock2.getpeername(), ":", host)
-                        sock2.sendall(data)
+                    # if host == specific_host:
+                    #     print(sock.getpeername(), ":", sock2.getpeername(), ":", host)
+                    #     sock2.sendall(data)
+                    sock2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    sock2.connect((host, 80))
 
                 if sock2 in r:
                     data = sock2.recv(chunk_size)
@@ -87,9 +97,10 @@ class TCPBridge(object):
                 (sock, addr) = self.server.accept()
                 if sock is None:
                     continue
-                client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                client_socket.connect((self.dst_host, self.dst_port))
-                self.tunnel(sock, client_socket)
+                # client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                # client_socket.connect((self.dst_host, self.dst_port))
+                # self.tunnel(sock, client_socket)
+                self.tunnel(sock)
             except KeyboardInterrupt:
                 self.stop = True
             except TimeoutError as exp:
@@ -99,5 +110,6 @@ class TCPBridge(object):
 
 
 if __name__ == "__main__":
-    tcp_bridge = TCPBridge("0.0.0.0", 8080, "192.168.44.130", 80)
+    # tcp_bridge = TCPBridge("0.0.0.0", 8080, "192.168.44.130", 80)
+    tcp_bridge = TCPBridge("0.0.0.0", 8080)
     tcp_bridge.run()
